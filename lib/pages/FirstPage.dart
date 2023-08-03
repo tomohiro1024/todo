ef390a454fb8f9dc44b4e24aa781eaa8c999f6bb
tomohiro1024 <gongyezhihong732@gmail.com>
2023-08-03 14:07:07 +0900

    code整理

---
+++ lib/pages/FirstPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('test'),
+      ),
+      body: const Center(
+        child: Text('test'),
+      ),
+    );
+  }
+}
